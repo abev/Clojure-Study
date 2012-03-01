01a1cc5f20b9406e72e65113adcfafcb6519b3b6
Sunil <abe_v@indiatimes.com>
2012-03-01 23:04:15 +0530

    Update factorial.clj

--- factorial.clj
+++ factorial.clj
@@ -1,5 +1,11 @@
 ;; returns the factorial of a number
-;; uses loop and recur 
+;; uses loop and recur
+;; this is bit of imperative coding as compared to declarative functional coding
+;; as in Haskell which supports like stating rules
+;; let fact n = 
+;;    n=1 :  1
+;;    n : n * fact n             ;; syntax may be not correct in Haskell ... 
+
  (defn factorial [n] 
   (loop [result 1  counter n]
       (if (= counter 1) 
